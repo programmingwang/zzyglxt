bc651b011c7d9843f54869253a6e449a26d95e74
huwenjin <1281820493@qq.com>
2020-10-28 16:39:47 +0800

    初次上传，项目整体结构搭建

---
+++ src/main/java/com/zyyglxt/ZyyglxtApplication.java
@@ -0,0 +1,13 @@
+package com.zyyglxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyyglxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyyglxtApplication.class, args);
+    }
+
+}

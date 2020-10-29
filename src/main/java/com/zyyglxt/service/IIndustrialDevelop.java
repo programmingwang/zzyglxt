2ca1b8ded46ec11178c2110ef1b7b5e3594dbda8
lrt123 <1250983138@qq.com>
2020-10-29 09:18:25 +0800

    更新配置

---
+++ src/main/java/com/zyyglxt/service/IIndustrialDevelop.java
@@ -0,0 +1,15 @@
+package com.zyyglxt.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author lrt
+ * @Date 2020/10/29 9:11
+ * @Version 1.0
+ **/
+@Service
+public interface IIndustrialDevelop {
+
+
+
+}
---
+++ src/main/java/com/zyyglxt/service/impl/IndustrialDevelopImpl.java
@@ -0,0 +1,9 @@
+package com.zyyglxt.service.impl;
+
+/**
+ * @Author lrt
+ * @Date 2020/10/29 9:11
+ * @Version 1.0
+ **/
+public class IndustrialDevelopImpl {
+}

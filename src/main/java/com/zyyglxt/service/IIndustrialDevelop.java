cc690fa4f0fe5829189178166ec32fdfd07d5402
lrt123 <1250983138@qq.com>
2020-10-29 11:45:48 +0800

    1、修改IndustrialDevelopSciAchiDO、IndustrialDevelopSciAchiDOKey为lombok形式,便于调试 2、增加service文件夹,增加IIndustrialDevelop接口以及实现 3、增加controller文件夹,增加IndustrialController 4、修改IndustrialDevelopSciAchiDOMapper,增加生成UUID

--- src/main/java/com/zyyglxt/service/IIndustrialDevelop.java
+++ src/main/java/com/zyyglxt/service/IIndustrialDevelop.java
@@ -1,15 +1,21 @@
 package com.zyyglxt.service;
 
-import org.springframework.stereotype.Service;
+import com.zyyglxt.dataobject.IndustrialDevelopSciAchiDO;
+import com.zyyglxt.dataobject.IndustrialDevelopSciAchiDOKey;
 
 /**
  * @Author lrt
  * @Date 2020/10/29 9:11
  * @Version 1.0
  **/
-@Service
 public interface IIndustrialDevelop {
 
+    void addAchievement(IndustrialDevelopSciAchiDO achiDO);
 
+    void delAchievement(IndustrialDevelopSciAchiDOKey key);
+
+    void updAchievement(IndustrialDevelopSciAchiDO achiDO);
+
+    void increaseVisitNum(IndustrialDevelopSciAchiDOKey key);
 
 }
--- src/main/java/com/zyyglxt/service/impl/IndustrialDevelopImpl.java
+++ src/main/java/com/zyyglxt/service/impl/IndustrialDevelopImpl.java
@@ -1,9 +1,41 @@
 package com.zyyglxt.service.impl;
 
+import com.zyyglxt.dao.IndustrialDevelopSciAchiDOMapper;
+import com.zyyglxt.dataobject.IndustrialDevelopSciAchiDO;
+import com.zyyglxt.dataobject.IndustrialDevelopSciAchiDOKey;
+import com.zyyglxt.service.IIndustrialDevelop;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 /**
  * @Author lrt
  * @Date 2020/10/29 9:11
  * @Version 1.0
  **/
-public class IndustrialDevelopImpl {
+@Service
+public class IndustrialDevelopImpl implements IIndustrialDevelop {
+
+    @Resource
+    IndustrialDevelopSciAchiDOMapper sciAchiDOMapper;
+
+    @Override
+    public void addAchievement(IndustrialDevelopSciAchiDO achiDO) {
+        sciAchiDOMapper.insertSelective(achiDO);
+    }
+
+    @Override
+    public void delAchievement(IndustrialDevelopSciAchiDOKey key) {
+        sciAchiDOMapper.deleteByPrimaryKey(key);
+    }
+
+    @Override
+    public void updAchievement(IndustrialDevelopSciAchiDO achiDO) {
+        sciAchiDOMapper.updateByPrimaryKeySelective(achiDO);
+    }
+
+    @Override
+    public void increaseVisitNum(IndustrialDevelopSciAchiDOKey key) {
+        sciAchiDOMapper.updateVisitNumByItemidAndItemcode(key);
+    }
 }

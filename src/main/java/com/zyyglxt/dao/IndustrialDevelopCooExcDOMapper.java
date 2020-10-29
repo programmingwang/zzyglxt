9fb22634fa62a71f366052fa49e32dae59f65533
lrt123 <1250983138@qq.com>
2020-10-29 17:55:55 +0800

    1、合作交流增删改 2、专家信息增删改 3、课题数据增删改 4、人才招聘增删改 5、填报时间数据增删改 6、在对应的mapper中都添加了生成uuid的sql

--- src/main/java/com/zyyglxt/dao/IndustrialDevelopCooExcDOMapper.java
+++ src/main/java/com/zyyglxt/dao/IndustrialDevelopCooExcDOMapper.java
@@ -51,4 +51,6 @@ public interface IndustrialDevelopCooExcDOMapper {
      * @mbg.generated Wed Oct 28 16:25:52 CST 2020
      */
     int updateByPrimaryKey(IndustrialDevelopCooExcDO record);
+
+    int updateVisitNumByItemidAndItemcode(IndustrialDevelopCooExcDOKey key);
 }

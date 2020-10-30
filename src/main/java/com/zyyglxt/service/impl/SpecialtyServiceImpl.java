bd9d1a0950c6630e3fdde50b9014704812ece8a7
qjc <2814964371@qq.com>
2020-10-30 17:29:53 +0800

    10.30下午

--- src/main/java/com/zyyglxt/service/impl/SpecialtyServiceImpl.java
+++ src/main/java/com/zyyglxt/service/impl/SpecialtyServiceImpl.java
@@ -7,6 +7,7 @@ import com.zyyglxt.dataobject.HospSpecialtyRefDOKey;
 import com.zyyglxt.dataobject.SpecialtyDO;
 import com.zyyglxt.dataobject.SpecialtyDOKey;
 import com.zyyglxt.service.ISpecialtyService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
  * @version 1.0
  * @date 2020/10/29 17:32
  */
+@Service
 public class SpecialtyServiceImpl implements ISpecialtyService {
 
     @Resource
@@ -23,24 +25,30 @@ public class SpecialtyServiceImpl implements ISpecialtyService {
     @Resource
     HospSpecialtyRefDOMapper hospSpecialtyRefDOMapper;
 
+    /*在控制层将数据封装好再传进service层*/
+
+    /*增加科室，同时也将科室与医院记录插入*/
     @Override
     public void addSpecialty(SpecialtyDO specialtyDO, HospSpecialtyRefDO hospSpecialtyRefDO) {
         specialtyDOMapper.insertSelective(specialtyDO);
         hospSpecialtyRefDOMapper.insertSelective(hospSpecialtyRefDO);
     }
 
+    /*更新科室信息，同步更新医院科室关系表*/
     @Override
     public void updateSpecialty(SpecialtyDO specialtyDO, HospSpecialtyRefDO hospSpecialtyRefDO) {
         specialtyDOMapper.updateByPrimaryKeySelective(specialtyDO);
         hospSpecialtyRefDOMapper.updateByPrimaryKeySelective(hospSpecialtyRefDO);
     }
 
+    /*删除科室记录，包括科室表和关系表*/
     @Override
     public void deleteSpecialty(SpecialtyDOKey specialtyDOKey, HospSpecialtyRefDOKey hospSpecialtyRefDOKey) {
         hospSpecialtyRefDOMapper.deleteByPrimaryKey(hospSpecialtyRefDOKey);
         specialtyDOMapper.deleteByPrimaryKey(specialtyDOKey);
     }
 
+    /*查询所有科室*/
     @Override
     public List<SpecialtyDO> selectAllSpecialty() {
         return specialtyDOMapper.selectAllSpecialty();
@@ -54,6 +62,7 @@ public class SpecialtyServiceImpl implements ISpecialtyService {
         return specialtyDOMapper.searchSpecialty(keyWord);
     }
 
+    /*查询前五条记录*/
     @Override
     public List<SpecialtyDO> top5Specialty() {
         return specialtyDOMapper.top5Specialty();

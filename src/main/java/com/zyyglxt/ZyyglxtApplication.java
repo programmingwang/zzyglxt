3abc6c5adaec193ca59713558d117516048a6a70
huangwanjin <459278264@qq.com>
2020-11-01 17:29:44 +0800

    优化代码

--- src/main/java/com/zyyglxt/ZyyglxtApplication.java
+++ src/main/java/com/zyyglxt/ZyyglxtApplication.java
@@ -3,6 +3,7 @@ package com.zyyglxt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @MapperScan("com.zyyglxt.dao")

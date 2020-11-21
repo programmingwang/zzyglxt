5220ba87a13a840856254363e54eced1d1710aa6
programmingwang <64132721+programmingwang@users.noreply.github.com>
2020-11-21 11:28:54 +0800

    #11.21上午 产业发展课题表新增字段PROJECT_NO，初步完成了集中评审

--- src/main/resources/static/project/js/scientificProject/centralizedReview.js
+++ src/main/resources/static/project/js/scientificProject/centralizedReview.js
@@ -1,10 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>$Title$</title>
-</head>
-<body>
-$END$
-</body>
-</html>
+(function () {
+    require(['jquery', 'ajaxUtil','bootstrapTableUtil','objectUtil','alertUtil','modalUtil','selectUtil','stringUtil','dictUtil'],
+        function (jquery,ajaxUtil,bootstrapTableUtil,objectUtil,alertUtil,modalUtil,selectUtil,stringUtil,dictUtil) {
+
+            var url = "/industrialdevelop/topic";
+
+            //角色加载工具
+
+            var aParam = {
+
+            };
+
+
+
+            //操作
+            function operation(value, row, index){
+                return [
+                    '<a class="view" style="margin:0 1em;text-decoration: none;color:#775637;" data-toggle="modal" data-target="" >查看</a>',
+                ].join('');
+            }
+
+
+
+            //修改事件
+            window.orgEvents = {
+                'click .view' : function (e, value, row, index) {
+                    var myViewTravelModalData ={
+                        modalBodyID : "myViewCulturalModal", //公用的在后面给span加不同的内容就行了，其他模块同理
+                        modalTitle : "查看详情",
+                        modalClass : "modal-lg",
+                        confirmButtonStyle: "display:none",
+                    };
+                    var myTravelModal = modalUtil.init(myViewTravelModalData);
+                    $("#chineseCulturalName").val(row.chineseCulturalName);
+                    $("#chineseCulturalSource").val(row.chineseCulturalSource);
+                    $("#chineseCulturalAuthor").val(row.chineseCulturalAuthor);
+                    $("#chineseCulturalContent").html(row.chineseCulturalContent);
+                    $("#creater").val(row.creater);
+                    $("#itemCreateAt").val(row.itemcreateat);
+                    $("#chineseCulturalStatus").val(webStatus[row.chineseCulturalStatus].text);
+                    $("#culturalImg").attr("src",row.filePath)
+                    $('#culturalImgSpan').html("景点图片");
+                    $('#culturalNameSpan').html("景点名称");
+                    $('#culturalContentSpan').html("景点介绍");
+
+                    myTravelModal.show();
+                },
+            };
+
+
+            var pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.showStatus);
+            $("#chargePersonSearch").selectUtil(pl);
+
+
+            var aCol = [
+                {field: 'projectNo', title: '项目编号'},
+                {field: 'projectName', title: '项目名称'},
+                {field: 'company', title: '申报单位'},
+                {field: 'status', title: '状态'},
+                {field: 'action',  title: '操作',formatter: operation,events:orgEvents}
+            ];
+
+            var myTable = bootstrapTableUtil.myBootStrapTableInit("table", url, aParam, aCol);
+
+            function refreshTable() {
+                var param = {};
+                myTable.free();
+                myTable = bootstrapTableUtil.myBootStrapTableInit("table", url, param, aCol);
+            }
+
+
+            bootstrapTableUtil.globalSearch("table",url,aParam, aCol);
+
+            var allTableData = $("#table").bootstrapTable("getData");
+            //console.log(allTableData);
+            localStorage.setItem('2',JSON.stringify(allTableData))
+            obj2=JSON.parse(localStorage.getItem("2"));
+        })
+})();
+

a592048eacc6f05df117a313c3070a667a503fb8
lrt123 <1250983138@qq.com>
2020-12-18 15:04:00 +0800

    中医医疗机构更新
    Signed-off-by: lrt123 <1250983138@qq.com>

--- src/main/resources/static/project/js/index.js
+++ src/main/resources/static/project/js/index.js
@@ -532,7 +532,7 @@
                     },
                     {
                         menu_name: "信息维护",
-                        menu_url: "/industrialdevelop/organization/addHosp",
+                        menu_url: "/industrialdevelop/organization/hosp_add",
                         id: "4",
                         level: "1",
                         pid: ""

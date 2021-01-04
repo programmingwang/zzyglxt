6ac40ef6d2b19bdf672de9072abda68c78f39d39
Cigarette-After-Sex <836644834@qq.com>
2021-01-04 19:28:37 +0800

    2021.1.4 内部会签后端

--- src/main/java/com/zyyglxt/config/WebSecurityConfig.java
+++ src/main/java/com/zyyglxt/config/WebSecurityConfig.java
@@ -88,7 +88,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/lab_add",
                 "/tecservice_add",
                 "/tour_add").permitAll().
-                   anyRequest().authenticated().
+                   //anyRequest().authenticated().
                     and().logout().
                     permitAll().//允许所有用户
                     logoutSuccessHandler(logoutSuccessHandler).//登出成功处理逻辑
---
+++ src/main/java/com/zyyglxt/dataobject/GovernresCountersign.java
@@ -0,0 +1,177 @@
+package com.zyyglxt.dataobject;
+
+import java.util.Date;
+
+/**
+    * 会签记录表
+    */
+public class GovernresCountersign {
+    /**
+    * 自增id
+    */
+    private Integer itemid;
+
+    /**
+    * UUID
+    */
+    private String itemcode;
+
+    /**
+    * 文件标题
+    */
+    private String receivingTitle;
+
+    /**
+    * 需要份数
+    */
+    private Integer number;
+
+    /**
+    * 政府信息公开
+    */
+    private String govPunlic;
+
+    /**
+    * 不公开理由
+    */
+    private String reason;
+
+    /**
+    * 文件编号
+    */
+    private String fileNo;
+
+    /**
+    * 密级
+    */
+    private String classification;
+
+    /**
+    * 数据状态
+    */
+    private String status;
+
+    /**
+    * 创建者
+    */
+    private String creater;
+
+    /**
+    * 创建时间
+    */
+    private Date itemcreateat;
+
+    /**
+    * 修改人
+    */
+    private String updater;
+
+    /**
+    * 修改时间
+    */
+    private Date itemupdateat;
+
+    public Integer getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(Integer itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getItemcode() {
+        return itemcode;
+    }
+
+    public void setItemcode(String itemcode) {
+        this.itemcode = itemcode;
+    }
+
+    public String getReceivingTitle() {
+        return receivingTitle;
+    }
+
+    public void setReceivingTitle(String receivingTitle) {
+        this.receivingTitle = receivingTitle;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getGovPunlic() {
+        return govPunlic;
+    }
+
+    public void setGovPunlic(String govPunlic) {
+        this.govPunlic = govPunlic;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getFileNo() {
+        return fileNo;
+    }
+
+    public void setFileNo(String fileNo) {
+        this.fileNo = fileNo;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreater() {
+        return creater;
+    }
+
+    public void setCreater(String creater) {
+        this.creater = creater;
+    }
+
+    public Date getItemcreateat() {
+        return itemcreateat;
+    }
+
+    public void setItemcreateat(Date itemcreateat) {
+        this.itemcreateat = itemcreateat;
+    }
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater;
+    }
+
+    public Date getItemupdateat() {
+        return itemupdateat;
+    }
+
+    public void setItemupdateat(Date itemupdateat) {
+        this.itemupdateat = itemupdateat;
+    }
+}

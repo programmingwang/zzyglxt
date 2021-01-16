05cc74296cd541a9a3e7520db2d9e822c0dfe600
programmingwang <64132721+programmingwang@users.noreply.github.com>
2021-01-17 01:10:36 +0800

    1.16 网页查询优化，整合成了工具

--- src/main/resources/static/project/js/industrialdevelop/audit.js
+++ src/main/resources/static/project/js/industrialdevelop/audit.js
@@ -348,10 +348,19 @@
                 bootstrapTableUtil.myBootStrapTableInit("table", url, param, aCol);
             });
 
-            var pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.showStatus);
-            $("#chargePersonSearch").selectUtil(pl);
-            pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.orgType);
-            $("#orgTypeSelect").selectUtil(pl);
+            var pl;
+            if (rolename == "产业发展-市级"){
+                pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.auditStatus);
+                $("#chargePersonSearch").selectUtil(pl);
+                pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.orgType);
+                $("#orgTypeSelect").selectUtil(pl);
+            }else if (rolename == "产业发展-省级"){
+                pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.auditStatus);
+                $("#chargePersonSearch").selectUtil(pl);
+                pl = dictUtil.getDictByCode(dictUtil.DICT_LIST.orgType);
+                $("#orgTypeSelect").selectUtil(pl);
+            }
+
 
             //增加全部选项
             var option = $("<option />");
@@ -397,6 +406,65 @@
                 myTable = bootstrapTableUtil.myBootStrapTableInit("table", getUrl, param, aCol);
             }
 
-            bootstrapTableUtil.globalSearch("table",getUrl,aParam, aCol);
+            // bootstrapTableUtil.globalSearch("table",getUrl,aParam, aCol,"status");
+            $("#btnSearch").unbind().on('click',function() {
+                var newArry = [];
+                var addstr=document.getElementById("chargePersonSearch").value;
+                switch (addstr) {
+                    case "0" : addstr = "待审核"; break;
+                    case "1" : addstr = "审核已通过"; break;
+                    case "2" : addstr = "审核未通过"; break;
+                }
+                var str = document.getElementById("taskNameSearch").value.toLowerCase();
+                var allTableData = JSON.parse(localStorage.getItem("2"));
+                if(str.indexOf("请输入")!=-1){
+                    str=""
+                }
+                for (var i in allTableData) {
+                    for (var v in aCol){
+                        var textP = allTableData[i][aCol[v].field];
+                        var isStatusSlot=false;           // 默认状态为true
+                        //状态条件判断,与表格字段的状态一致,这里根据自己写的修改
+                        var status= allTableData[i]["status"]
+                        // console.log("addstr:"+addstr)
+                        // console.log("status:"+status)
+                        //调试时可以先打印出来，进行修改
+                        if(addstr==status){
+                            isStatusSlot=true;
+                        }
+                        //当存在时将条件改为flase
+                        if (textP == null || textP == undefined || textP == '') {
+                            textP = "1";
+                        }
+                        if($("#closeAndOpen").text().search("展开")!= -1 && textP.search(str) != -1){
+                            isStatusSlot = false;
+                            newArry.push(allTableData[i])
+                        }
+                        if($("#closeAndOpen").text().search("收起")!= -1 && textP.search(str) != -1 && isStatusSlot){
+                            newArry.push(allTableData[i])
+                        }
+                    }
+                }
+                var newArr=new Set(newArry)
+                newArry=Array.from(newArr)
+                $("#table").bootstrapTable("load", newArry);
+                if(newArry.length == 0){
+                    alertUtil.warning("搜索成功,但此搜索条件下没有数据");
+                }else{
+                    alertUtil.success("搜索成功");
+                }
+            })
+
+            var aria=this.ariaExpanded;
+            $("#closeAndOpen").unbind().on('click',function(){
+                this.innerText="";
+                if (aria==="true"){
+                    this.innerText="展开";
+                    aria = "false";
+                } else {
+                    this.innerText="收起";
+                    aria = "true";
+                }
+            })
         })
 })();

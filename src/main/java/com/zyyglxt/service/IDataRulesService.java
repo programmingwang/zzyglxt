b5d729aeb663a3e3a763cec136874685912e551a
qjc <2814964371@qq.com>
2021-02-04 17:45:15 +0800

    2.4 规章制度功能完成

---
+++ src/main/java/com/zyyglxt/service/IDataRulesService.java
@@ -0,0 +1,27 @@
+package com.zyyglxt.service;
+
+import com.zyyglxt.dataobject.DataDO;
+import com.zyyglxt.dataobject.DataDOKey;
+import com.zyyglxt.dto.DataDto;
+
+import java.util.List;
+
+/**
+ * @author qjc
+ * @version 1.0
+ * @date 2021/2/4 14:02
+ */
+public interface IDataRulesService {
+    //增加一个规章制度
+    int insert(DataDO record);
+    //删除规章制度信息
+    int delete(DataDOKey dataDOKey);
+    //更新规章制度信息
+    int update(DataDO record);
+
+    //查询规章制度根据身份状态
+    List<DataDto> selectRules(String dataStatus);
+    //获取首页数据
+    List<String> selectForMainPage();
+
+}

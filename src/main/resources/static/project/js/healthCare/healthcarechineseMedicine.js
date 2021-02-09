c701673fa9316a1f24ae9b3838dd2222606833cd
zuohong <823178351@qq.com>
2021-02-09 21:13:03 +0800

    2.9 中药常识搜索报错解决

--- src/main/resources/static/project/js/healthCare/healthcarechineseMedicine.js
+++ src/main/resources/static/project/js/healthCare/healthcarechineseMedicine.js
@@ -349,13 +349,11 @@
                 var addstr=document.getElementById("chargePersonSearch").value;
                 var str = document.getElementById("taskNameSearch").value.toLowerCase();
                 var allTableData = JSON.parse(localStorage.getItem("2"));
-                var searchGxfl=document.getElementById("Search").value;
                 if(str.indexOf("请输入")!=-1){
                     str=""
                 }
                 for (var i in allTableData) {
                     for (var v in aCol){
-                        var gxfl= allTableData[i]["chineseMedicineType"]
                         var textP = allTableData[i][aCol[v].field];
                         var isStatusSlot=false;           // 默认状态为true
                         //状态条件判断,与表格字段的状态一致,这里根据自己写的修改
@@ -369,9 +367,7 @@
                         // console.log("status:"+status)
                         //调试时可以先打印出来，进行修改
                         if(addstr==status || addstr==99){
-                            if(gxfl == searchGxfl){
-                                isStatusSlot=true;
-                            }
+                            isStatusSlot=true;
                         }
                         if(typeof textP == "object") continue;
                         else if(typeof textP == "number") textP = textP.toString();

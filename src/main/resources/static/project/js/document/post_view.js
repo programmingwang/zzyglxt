634b7632f3eb2082001f59d7c093996bc303644c
htmj <14626966htmj>
2021-04-11 00:49:33 +0800

    调整红头文件

--- src/main/resources/static/project/js/document/post_view.js
+++ src/main/resources/static/project/js/document/post_view.js
@@ -332,7 +332,7 @@
                             submitStatus.postOpinion = "2";
                         } else if (rolename === "中医处分管局长" || rolename === "中药处分管局长" || rolename === "综合处分管局长" || rolename === "法规监督处分管局长") {
                             $("#deputyDirectorView").validate("审核意见");
-                            submitStatus.postDataStatus = postStatus[6].id;
+                            //submitStatus.postDataStatus = postStatus[6].id;
                             if (rolename === "中医处分管局长") {
                                 submitOpinion = {
                                     "dataCode": tempdata.itemcode,
@@ -361,6 +361,106 @@
                                     "deputyDirectorName3": username,
                                     "deputyDirectorDate3": nowTime,
                                 };
+                            }
+                            if (tempdata.postOpinion1 === "1"){
+                                if (!tgAdvice.data.deputyDirector){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "2") {
+                                if (!tgAdvice.data.deputyDirector1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "3") {
+                                if (!tgAdvice.data.deputyDirector2){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "4") {
+                                if (!tgAdvice.data.deputyDirector3){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "12") {
+                                var po12 =[];
+                                if (!tgAdvice.data.deputyDirector){ po12.push("1");}
+                                if (!tgAdvice.data.deputyDirector1){ po12.push("1");}
+                                if (po12.length <= 1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "13") {
+                                var po13 =[];
+                                if (!tgAdvice.data.deputyDirector){ po13.push("1");}
+                                if (!tgAdvice.data.deputyDirector2){ po13.push("1");}
+                                if (po13.length <= 1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "14") {
+                                var po14 =[];
+                                if (!tgAdvice.data.deputyDirector){ po14.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po14.push("1");}
+                                if (po14.length <= 1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "23") {
+                                var po23 =[];
+                                if (!tgAdvice.data.deputyDirector1){ po23.push("1");}
+                                if (!tgAdvice.data.deputyDirector2){ po23.push("1");}
+                                if (po23.length <= 1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "24") {
+                                var po24 =[];
+                                if (!tgAdvice.data.deputyDirector1){ po24.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po24.push("1");}
+                                if (po24.length <= 1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "34") {
+                                var po34 =[];
+                                if (!tgAdvice.data.deputyDirector2){ po34.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po34.push("1");}
+                                if (po34.length <= 1){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "123") {
+                                var po123 =[];
+                                if (!tgAdvice.data.deputyDirector){ po123.push("1");}
+                                if (!tgAdvice.data.deputyDirector1){ po123.push("1");}
+                                if (!tgAdvice.data.deputyDirector2){ po123.push("1");}
+                                if (po123.length <= 2){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "124") {
+                                var po124 =[];
+                                if (!tgAdvice.data.deputyDirector){ po124.push("1");}
+                                if (!tgAdvice.data.deputyDirector1){ po124.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po124.push("1");}
+                                if (po124.length <= 2){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "134") {
+                                var po134 =[];
+                                if (!tgAdvice.data.deputyDirector){ po134.push("1");}
+                                if (!tgAdvice.data.deputyDirector2){ po134.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po134.push("1");}
+                                if (po134.length <= 2){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "234") {
+                                var po234 =[];
+                                if (!tgAdvice.data.deputyDirector1){ po234.push("1");}
+                                if (!tgAdvice.data.deputyDirector2){ po234.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po234.push("1");}
+                                if (po234.length <= 2){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
+                            }else if (tempdata.postOpinion1 === "1234") {
+                                var po1234 =[];
+                                if (!tgAdvice.data.deputyDirector){ po1234.push("1");}
+                                if (!tgAdvice.data.deputyDirector1){ po1234.push("1");}
+                                if (!tgAdvice.data.deputyDirector2){ po1234.push("1");}
+                                if (!tgAdvice.data.deputyDirector3){ po1234.push("1");}
+                                if (po1234.length <= 3){
+                                    submitStatus.postDataStatus = postStatus[6].id;
+                                }else {submitStatus.postDataStatus = postStatus[4].id;}
                             }
                             submitStatus.postOpinion = "3";
                         } else if (rolename === "政务资源局长") {
